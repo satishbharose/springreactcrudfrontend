b1fbe520cc6e79575455f353c5f33f78ef7aaafd
Satish <satishdbharose@hotmail.com>
2023-10-30 22:01:49 +0530

    Add files via upload
    add user in progress

---
+++ src/users/AddUser.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+export default function AddUser() {
+
+    const [user, setUser] = useState({ 
+        name:"",
+        username:"",
+        email:""
+     })
+
+     const { name, username, email } = user
+
+     const onInputChange = (e) => { 
+        setUser({...user, [e.target.name]:e.target.value})
+      }  
+
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col-md-6 offset-md-3 border rounded p-4 mt-2">
+                <h2 className="text-center m-4">Register User</h2>
+                <div className="mb-3">
+                    <label htmlFor="Name" className="form-label">
+                        Name
+                    </label>
+                    <input type={"text"}
+                           className="form-control"
+                           placeholder="Enter your name"
+                           name = "name" 
+                           value= {name} 
+                           onChange={(e) => onInputChange(e)} 
+                    />
+                    <label htmlFor="Username" className="form-label">
+                        Username
+                    </label>       
+                    <input type={"text"}
+                           className="form-control"
+                           placeholder="Enter your username"
+                           name = "username"
+                           value= {username} 
+                           onChange={(e) => onInputChange(e)}        
+                    />
+                    <label htmlFor="Email" className="form-label">
+                        E-mail
+                    </label>
+                    <input type={"text"}
+                           className="form-control"
+                           placeholder="Enter your email address"
+                           name = "email"
+                           value= {email} 
+                           onChange={(e) => onInputChange(e)}       
+                    />   
+                </div>
+                <button type="submit" className="btn btn-outline-primary">Submit</button>
+                <button type="submit" className="btn btn-outline-danger mx-2">Cancel</button>
+            </div>
+        </div>
+    </div>
+  )
+}
